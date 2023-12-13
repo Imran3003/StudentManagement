2591ae0133629c6917e65141a311f7880c73928d
Mohamed Subaideen Imran <imranmohamed783@gmail.com>
2023-12-13 14:38:35 +0530

    new changeList

---
+++ SrudentManagement-BE/src/main/java/com/ib/stm/StudentManagementBEApplication.java
@@ -0,0 +1,13 @@
+package com.ib.stm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementBEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementBEApplication.class, args);
+    }
+
+}
---
+++ SrudentManagement-BE/src/test/java/com/ib/stm/StudentManagementBEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.stm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementBEApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
